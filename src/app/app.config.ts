89c9e5596bf2927b072d3f70e2d4f291f0ae607e
kameera135 <kameera135@users.noreply.github.com>
2024-11-18 08:44:35 +0530

    Make interceptor work

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,17 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { authInterceptor } from './auth/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    ),
+  ],
 };
